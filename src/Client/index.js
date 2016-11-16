572dcaa24d054963cbbe48e0ba774ad11e5eae15
Tymoteusz Kapuściński <tymoteusz.kapuscinski@sabre.com>
2016-11-16 17:10:15 +0100

    Working sync between multiple browsers

--- src/Client/index.js
+++ src/Client/index.js
@@ -1,67 +1,113 @@
-import CanvasRenderer from '~/Pixi/core/renderers/canvas/CanvasRenderer'
-import Container from '~/Pixi/core/display/Container'
-import Texture from '~/Pixi/core/textures/Texture'
-import SpriteRenderer from '~/Pixi/core/sprites/canvas/CanvasSpriteRenderer'
-import Sprite from '~/Pixi/core/sprites/Sprite'
+import * as PIXI from '~/Pixi'
 
 import World from './World'
 import NetworkHandler from './NetworkHandler'
 import PlayerAvatar from './PlayerAvatar'
 
-const renderer = new CanvasRenderer(800, 600, {backgroundColor : 0x000000});
+PIXI.SCALE_MODES.DEFAULT = PIXI.SCALE_MODES.NEAREST;
+const renderer = new PIXI.CanvasRenderer(800, 600, {backgroundColor : 0x000000});
 
 document.body.appendChild(renderer.view);
 
 // create the root of the scene graph
-const stage = new Container();
+const stage = new PIXI.Container();
+stage.scale.x = 2;
+stage.scale.y = 2;
 
 const world = new World(stage);
 
-const texture = Texture.fromImage('assets/bunny.png');
+const texture = PIXI.Texture.fromImage('assets/bunny.png');
 
-// create a new Sprite using the texture
-const bunny = new Sprite(texture);
-
-// center the sprite's anchor point
-bunny.anchor.x = 0.5;
-bunny.anchor.y = 0.5;
-
-// move the sprite to the center of the screen
-bunny.position.x = Math.floor(Math.random()*800);
-bunny.position.y = Math.floor(Math.random()*600);
-
-stage.addChild(bunny);
+let myBunny = false;
 
 // start animating
 animate();
 function animate() {
     requestAnimationFrame(animate);
 
-    // just for fun, let's rotate mr rabbit a little
-    bunny.rotation += 0.1;
+    if(myBunny){
+      // just for fun, let's rotate mr rabbit a little
+      myBunny.rotation += 0.1;
+    }
 
     // render the container
     renderer.render(stage);
 }
 
-bunny.interactive = true;
-bunny.on('mousedown', onDown);
-bunny.on('touchstart', onDown);
+function createBunny(pos, mine){
+  // create a new Sprite using the texture
+  const bunny = new PIXI.Sprite(texture);
 
-function onDown (eventData) {
-    bunny.scale.x += 0.3;
-    bunny.scale.y += 0.3;
+  // center the sprite's anchor point
+  bunny.anchor.x = 0.5;
+  bunny.anchor.y = 0.5;
+
+  // move the sprite to the center of the screen
+  bunny.position.x = pos[0];
+  bunny.position.y = pos[1];
+
+  bunny.tint = 0xFFAAAA;
+
+  if(mine){
+    myBunny = bunny;
+    bunny.tint = 0xAAFFAA;
+    function onDown (eventData) {
+      console.log('click');
+      bunny.scale.x += 0.3;
+      bunny.scale.y += 0.3;
+    }
+
+    bunny.interactive = true;
+    bunny.on('mousedown', onDown);
+    bunny.on('touchstart', onDown);
+  }
+
+  stage.addChild(bunny);
+  return bunny;
 }
 
+const bunnies = {};
 
 var host = window.document.location.host.replace(/:.*/, '');
 var ws = new WebSocket('ws://' + host + ':3000');
 ws.onmessage = function (event) {
-  console.log(event);
-  console.log(JSON.parse(event.data));
+  const json = JSON.parse(event.data);
+  console.log(json, bunnies);
+  if(json.type==='login'){
+    const newbun = createBunny(json.pos, true);
+    bunnies[json.id] = {
+      id: json.id,
+      pos: json.pos,
+      sprite: newbun
+    }
+
+    for(var i in json.others){
+      const newbunz = createBunny(json.others[i].pos);
+      bunnies[json.others[i].id] = {
+        id: json.others[i].id,
+        pos: json.others[i].pos,
+        sprite: newbunz
+      }
+    }
+  }
+
+  if(json.type==='remove'){
+    const bun = bunnies[json.id];
+    stage.removeChild(bun.sprite);
+    delete bunnies[json.id];
+  }
+
+  if(json.type==='add'){
+    const newbunz = createBunny(json.pos);
+    bunnies[json.id] = {
+      id: json.id,
+      pos: json.pos,
+      sprite: newbunz
+    }
+  }
+  console.log(json, bunnies);
 };
+
 const keepAliveInterval = setInterval(function () {
-  ws.send('asdasdasd', {}, function(res){
-    console.log('send callback', res)
-  });
-}, 1000);
+  ws.send('{}');
+}, 15000);
--- src/Server/ClientConnection.js
+++ src/Server/ClientConnection.js
@@ -15,8 +15,16 @@ export default class ClientConnection {
   static registerConnection(client){
     ClientConnection.connections[client.id] = client;
   }
+
   static unregisterConnection(client){
     delete ClientConnection.connections[client.id];
+  }
+
+  static broadcastMessage(message, excludeConnection){
+    for(var i in ClientConnection.connections){
+      if(ClientConnection.connections[i]===excludeConnection) continue;
+      ClientConnection.connections[i].send(message)
+    }
   }
 
   messageQueue = []
@@ -25,13 +33,40 @@ export default class ClientConnection {
     this.ws = ws;
     this.lastMessage = new Date().getTime();
     this.id = ClientConnection.getNewID();
+    this.pos = [Math.floor(Math.random()*400), Math.floor(Math.random()*300)];
     ClientConnection.registerConnection(this);
+    console.log(this.id+'| open');
 
     ws.on('close', () => {
       console.log(this.id+'| closed');
       ClientConnection.unregisterConnection(this);
+      ClientConnection.broadcastMessage({
+        type: 'remove',
+        id: this.id,
+        pos: this.pos
+      })
     });
     ws.on('message', this.onMessage);
+    //reply with the current state of board
+    const others = [];
+    for(var i in ClientConnection.connections){
+      if(ClientConnection.connections[i]===this) continue;
+      others.push({
+        id: ClientConnection.connections[i].id,
+        pos: ClientConnection.connections[i].pos
+      });
+    }
+    this.send({
+      type: 'login',
+      id: this.id,
+      pos: this.pos,
+      others: others
+    })
+    ClientConnection.broadcastMessage({
+      type: 'add',
+      id: this.id,
+      pos: this.pos
+    }, this)
   }
 
   heartBeat = () => {
@@ -40,25 +75,26 @@ export default class ClientConnection {
 
   keepAlive = () => {
     const now = new Date().getTime();
-    if(now - this.lastMessage > 2*1000) {
-      this.send({keepAlive:true});
+    if(now - this.lastMessage > 20*1000) {
+      this.send({});
     }
   }
 
   send = (data) => {
     this.lastMessage = new Date().getTime();
     try{
+      console.log(data);
       this.ws.send(JSON.stringify(data), () => {
-        console.log(this.id+'| send callback');
+        //console.log(this.id+'| send callback');
       });
     } catch(err) {
-      console.log(err);
+      console.log(this.id+'| Error: ', err);
     }
   }
 
   onMessage = (message) => {
     this.lastMessage = new Date().getTime();
+    ClientConnection.broadcastMessage(message, this);
     console.log(this.id+'| message(last: '+this.lastMessage+'): ', message);
-    this.send({'reply':''});
   }
 }
